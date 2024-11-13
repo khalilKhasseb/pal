3e9381b8612c8d30406b81c38882ce336bef9367
khalil <khalil@palgbc.a2hosted.com>
2024-11-13 10:43:00 +0200

    Typo

--- resources/views/vendor/zeus/themes/zeus/sky/partial/templates/tag-members.blade.php
+++ resources/views/vendor/zeus/themes/zeus/sky/partial/templates/tag-members.blade.php
@@ -1,3 +1,18 @@
+@use(Illuminate\Pagination\Paginator)
+@php
+$current_page =  !isset(request()->query()['page']) || empty(request()->query()) ? 1 : (int) request()->query()['page'] ;
+$per_page = 15;
+
+$offset = ($current_page * $per_page) - $per_page ;
+
+$totalPages = ceil($posts->count()/$per_page) ;
+
+$paginiator = app(Paginator::class, ['items' => $posts->slice($offset,$per_page), 'perPage' =>$per_page]);
+/** this to indicate all pages needed for all itesm in a paginaitor */
+$hasMorePages = $totalPages > $current_page ;
+
+// get Pages for a period of offsets
+// for example if we have
 $paginiator->hasMorePagesWhen($hasMorePages)->withPath(request()->path());
 // $paginiator->path('blog') ;
 @endphp
