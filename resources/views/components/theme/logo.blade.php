fe7386699b5fa7bb4931084bfbb4c8ead2415a7e
khalilKhasseb <khalil.khassep@gmail.com>
2025-01-30 22:37:24 +0200

    Update logo.blade.php

--- resources/views/components/theme/logo.blade.php
+++ resources/views/components/theme/logo.blade.php
@@ -1,29 +1,24 @@
 @props([
-'url' => null,
-'route' => '/'
+    'url' => null,
+    'route' => '/',
 ])
 
 @php
-   
-    if(!Storage::disk('public')->exists('/site\/'.basename($url))
-    ) {
-       
-        if(session()->has('somoud_load'))
-        {
+
+    if (!Storage::disk('public')->exists('/site\/' . basename($url))) {
+        dd($url);
+        if (session()->has('somoud_load')) {
             $url = config('theme.samoud_logo');
-        }else{
-            $url = config('theme.console_logo');        
+        } else {
+            $url = config('theme.console_logo');
         }
-       
     }
 
-    
-    
 @endphp
 
 
 {{-- Recheck optional Pregmatch --}}
-<a class="navbar-brand" href="{{$route}}">
-    <img src="{{preg_match('/(http?s)/' , $url) ? $url : asset('storage/'.$url)}}" alt="logo"
+<a class="navbar-brand" href="{{ $route }}">
+    <img src="{{ preg_match('/(http?s)/', $url) ? $url : asset('storage/' . $url) }}" alt="logo"
         class="img-responsive" />
-    </a>
+</a>
